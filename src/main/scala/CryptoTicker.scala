afb2e3d908eda9e41d60c38193bfda51f677466f
Javier <kvltd00d@gmail.com>
2021-09-09 10:04:51 -0400

    added a putStrLn

--- src/main/scala/CryptoTicker.scala
+++ src/main/scala/CryptoTicker.scala
@@ -12,10 +12,9 @@ import zio.duration._
 import zio.stream.ZStream
 import zio.clock.Clock
 import zio.console.Console.Service
+import java.io.IOException
 
 object CryptoTicker extends App {
-
-  def msg = "I was compiled by Scala 3 and I'm using ZIO 2.0. :)"
 
   def ticketURLCall(symbol: String): UIO[java.math.BigDecimal] = ZIO.succeed {
     YahooFinance.get(symbol).getQuote(true).getPrice
@@ -27,11 +26,17 @@ object CryptoTicker extends App {
 
   def stringMsg(symbol: String, price: java.math.BigDecimal) = WebSocketFrame.text(s"$symbol: $price")
 
-  private val socket: Socket[Has[zio.clock.Clock.Service], Nothing, WebSocketFrame, WebSocketFrame] = Socket.collect[WebSocketFrame] {
-    case WebSocketFrame.Text(symbol)  =>
-      ZStream
-        .repeatEffect(ticketURLCall(symbol).map(price => stringMsg(symbol, price)))
-        .throttleShape(1, 3.seconds)(_ => 1) //.map(parsePrice) // in case we're parsing JSON we'd have a parsePrice function
+  private val socket: Socket[Has[Console.Service] & Has[Clock.Service], IOException, WebSocketFrame, WebSocketFrame] = 
+    Socket.collect[WebSocketFrame] {
+      case WebSocketFrame.Text(symbol)  =>
+        ZStream
+          .repeatEffect(
+            for {
+              result <- ticketURLCall(symbol).map(price => stringMsg(symbol, price))
+              _      <- putStrLn(result.toString)
+            } yield result
+          )
+          .throttleShape(1, 3.seconds)(_ => 1) //.map(parsePrice) // in case we're parsing JSON we'd have a parsePrice function
   }
 
   private val app = HttpApp.collect {
